3503dfd627cdac2e6614fdfc7213d54b1fd4cb76
Han <sihancheng03@gmail.com>
2025-03-25 16:36:13 -0700

    CLI tailwind css approach

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "./styles.css";
 import { AuthProvider } from '../lib/AuthContext';
 
 const geistSans = Geist({
